f00dbfa2dacc82fb33f40fdcaa10ef786107e32f
Stijn Verhoeven <stijn.verhoeven1@edu-kw1c.nl>
2020-02-12 11:29:49 +0100

    Opdracht 2.1 is klaar

---
+++ h2/Opdracht2.1.php
@@ -0,0 +1,18 @@
+<?php>
+/**
+ * User: Stijn Verhoeven
+ * Date: 12-02-2020
+ * Time: 10:53
+ * File: index.php
+ **/
+?>
+<header>
+
+</header>
+<body>
+    <p>
+        <?php
+        echo "Hello Word"
+        ?>
+    </p>
+</body>
